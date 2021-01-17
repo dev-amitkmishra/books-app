3d68aed495d033d6f8e3b46ffd69d37c4b3ecec1
amit.mishra <amit.mishra@soprasteria.com>
2021-01-18 03:04:16 +0530

    feat: test case added for rendering without crash

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,33 @@
-import { render, screen } from '@testing-library/react';
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/Books/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+import React from 'react';
+import { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { Provider } from 'react-redux';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+configure({ adapter: new Adapter() });
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+const initialState = {
+  books: [],
+  localBooks: []
+}
+it('renders without crashing', () => {
+  const store = mockStore(initialState);
+  shallow(
+    <Provider store={store}> 
+      <App />
+    </Provider>
+  );
 });
